4c2095757f10fb28d96938e7388a34173f6e54f3
Chuck Dries <chuck@chuckdries.com>
2023-07-21 19:08:52 -0700

    text input

--- src/App.js
+++ src/App.js
@@ -3,50 +3,62 @@ import { useGLTF, MeshReflectorMaterial, BakeShadows } from '@react-three/drei'
 import { EffectComposer, Bloom, DepthOfField } from '@react-three/postprocessing'
 import { easing } from 'maath'
 import { Instances, Computers } from './Computers'
+import { useRef, useState } from 'react'
 
 export default function App() {
+  const [name, setName] = useState('Poimandres')
+  const txt = useRef()
   return (
-    <Canvas shadows dpr={[1, 1.5]} camera={{ position: [-1.5, 1, 5.5], fov: 45, near: 1, far: 20 }} eventSource={document.getElementById('root')} eventPrefix="client">
-      {/* Lights */}
-      <color attach="background" args={['black']} />
-      <hemisphereLight intensity={0.15} groundColor="black" />
-      <spotLight position={[10, 20, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
-      {/* Main scene */}
-      <group position={[-0, -1, 0]}>
-        {/* Auto-instanced sketchfab model */}
-        <Instances>
-          <Computers scale={0.5} />
-        </Instances>
-        {/* Plane reflections + distance blur */}
-        <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
-          <planeGeometry args={[50, 50]} />
-          <MeshReflectorMaterial
-            blur={[300, 30]}
-            resolution={2048}
-            mixBlur={1}
-            mixStrength={80}
-            roughness={1}
-            depthScale={1.2}
-            minDepthThreshold={0.4}
-            maxDepthThreshold={1.4}
-            color="#202020"
-            metalness={0.8}
-          />
-        </mesh>
-        {/* Bunny and a light give it more realism */}
-        <Bun scale={0.325} position={[0, 0, 0.5]} rotation={[0, -Math.PI * 0.85, 0]} />
-        <pointLight distance={1.5} intensity={1} position={[-0.15, 0.7, 0]} color="orange" />
-      </group>
-      {/* Postprocessing */}
-      <EffectComposer disableNormalPass>
-        <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={6} />
-        <DepthOfField target={[0, 0, 13]} focalLength={0.3} bokehScale={15} height={700} />
-      </EffectComposer>
-      {/* Camera movements */}
-      <CameraRig />
-      {/* Small helper that freezes the shadows for better performance */}
-      <BakeShadows />
-    </Canvas>
+    <>
+      <Canvas
+        onFocus={() => txt.current?.focus()}
+        shadows
+        dpr={[1, 1.5]}
+        camera={{ position: [-1.5, 1, 5.5], fov: 45, near: 1, far: 20 }}
+        eventSource={document.getElementById('root')}
+        eventPrefix="client">
+        {/* Lights */}
+        <color attach="background" args={['black']} />
+        <hemisphereLight intensity={0.15} groundColor="black" />
+        <spotLight position={[10, 20, 10]} angle={0.12} penumbra={1} intensity={1} castShadow shadow-mapSize={1024} />
+        {/* Main scene */}
+        <group position={[-0, -1, 0]}>
+          {/* Auto-instanced sketchfab model */}
+          <Instances>
+            <Computers name={name} scale={0.5} />
+          </Instances>
+          {/* Plane reflections + distance blur */}
+          <mesh receiveShadow rotation={[-Math.PI / 2, 0, 0]}>
+            <planeGeometry args={[50, 50]} />
+            <MeshReflectorMaterial
+              blur={[300, 30]}
+              resolution={2048}
+              mixBlur={1}
+              mixStrength={80}
+              roughness={1}
+              depthScale={1.2}
+              minDepthThreshold={0.4}
+              maxDepthThreshold={1.4}
+              color="#202020"
+              metalness={0.8}
+            />
+          </mesh>
+          {/* Bunny and a light give it more realism */}
+          <Bun scale={0.325} position={[0, 0, 0.5]} rotation={[0, -Math.PI * 0.85, 0]} />
+          <pointLight distance={1.5} intensity={1} position={[-0.15, 0.7, 0]} color="orange" />
+        </group>
+        {/* Postprocessing */}
+        <EffectComposer disableNormalPass>
+          <Bloom luminanceThreshold={0} mipmapBlur luminanceSmoothing={0.0} intensity={6} />
+          <DepthOfField target={[0, 0, 13]} focalLength={0.3} bokehScale={15} height={700} />
+        </EffectComposer>
+        {/* Camera movements */}
+        <CameraRig />
+        {/* Small helper that freezes the shadows for better performance */}
+        <BakeShadows />
+      </Canvas>
+      <input ref={txt} autoFocus type="text" value={name} onChange={(e) => setName(e.target.value)} />
+    </>
   )
 }
 
